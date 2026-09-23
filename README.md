84e127df20036c69de3511a35d89bbe05de0c8d8
Veronica (veronoicc) <contact@veronoi.cc>
2026-09-23 20:14:56 +0200

    feat: add plugins
    Add SilentDelete, SilentEdit, UnlimitedAccounts, and clipGifSearch.
    
    Remove README.md.

--- README.md
+++
@@ -1,1 +0,0 @@
-# vencord-plugins
---
+++ UnlimitedAccounts/index.ts
@@ -0,0 +1,67 @@
+/*
+ * Vencord, a modification for Discord's desktop app
+ * Copyright (c) 2023 Vendicated and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+import { definePluginSettings } from "@api/Settings";
+import definePlugin, { OptionType } from "@utils/types";
+
+const settings = definePluginSettings({
+    maxAccounts: {
+        description: "Number of accounts that can be added, or 0 for no limit",
+        default: 0,
+        type: OptionType.NUMBER,
+        restartNeeded: true,
+    },
+});
+
+export default definePlugin({
+    name: "UnlimitedAccounts",
+    description: "Increases the amount of accounts you can add.",
+    authors: [{ name: "V", id: 315547631373778945n }],
+    settings,
+    patches: [
+        {
+            find: "o.A.getUsers()",
+            replacement: {
+                match: /maxNumAccounts:\s*(\d+)/,
+                replace: 'maxNumAccounts: $self.getMaxAccounts()',
+            },
+        },
+        {
+            find: "o.A.getUsers()",
+            replacement: {
+                match: /I.length\s*<\s*5/g,
+                replace: 'I.length < $self.getMaxAccounts()',
+            },
+        },
+        {
+            find: "o.A.getUsers()",
+            replacement: {
+                match: /I.length\s*>=\s*5/g,
+                replace: 'I.length >= $self.getMaxAccounts()',
+            },
+        },
+        {
+            find: "this.getCanUseMultiAccountMobile()",
+            replacement: {
+                match: /\(_\s*=\s*n\)\.length\s*>\s*5\s*&&\s*_\.splice\(5\)/g,
+                replace: '(_ = n).length > $self.getMaxAccounts() && _.splice($self.getMaxAccounts())',
+            },
+        },
+    ],
+    getMaxAccounts() { return settings.store.maxAccounts === 0 ? Infinity : settings.store.maxAccounts; },
+});
